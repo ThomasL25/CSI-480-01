a246124fe96a1ee21b055930035d70ebf38c4e1f
Justin Allman <126277294+JAProgramming@users.noreply.github.com>
2025-10-22 00:01:15 -0400

    Update JustinPage.jsx

--- Project2/Project2Website/BinaryDecisionTreeWebsite/src/pages/JustinPage.jsx
+++ Project2/Project2Website/BinaryDecisionTreeWebsite/src/pages/JustinPage.jsx
@@ -3,7 +3,7 @@ function JustinPage() {
         <div className="page-container">
             <h1>Justin's Page is for my visualations and analytics</h1>
             <h2>Visualization:</h2>
-            <img src="./Top15MeanByJurisdiction.png" alt= "Top 15 Mean By Jurisdicition"/>
+            <img src="Project2/Project2Website/BinaryDecisionTreeWebsite/src/assets/Top15MeanByJurisdiction.png" alt= "Top 15 Mean By Jurisdicition"/>
             <h2>Analytics</h2>
         </div>
     );
