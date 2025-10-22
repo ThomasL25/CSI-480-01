bb32a5881b1625ddc2dffcf506e041c229b92125
Justin Allman <126277294+JAProgramming@users.noreply.github.com>
2025-10-22 00:15:58 -0400

    Update JustinPage.jsx

--- Project2/Project2Website/BinaryDecisionTreeWebsite/src/pages/JustinPage.jsx
+++ Project2/Project2Website/BinaryDecisionTreeWebsite/src/pages/JustinPage.jsx
@@ -1,9 +1,11 @@
+import HighestMeans from './assets/Top15MeanByJurisdiction.png';
+
 function JustinPage() {
     return (
         <div className="page-container">
             <h1>Justin's Page is for my visualations and analytics</h1>
             <h2>Visualization:</h2>
-            <img src="./assets/Top15MeanByJurisdiction.png" alt= "Top 15 Mean By Jurisdicition"/>
+            <img src={HighestMeans} alt="Top 15 Mean By Jurisdicition"/>
             <h2>Analytics</h2>
         </div>
     );
