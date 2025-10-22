e4e36577d31dfa5fd80ace59a2e64db01317b6a9
Justin Allman <126277294+JAProgramming@users.noreply.github.com>
2025-10-22 11:07:03 -0400

    Update JustinPage.jsx

--- Project2/Project2Website/BinaryDecisionTreeWebsite/src/pages/JustinPage.jsx
+++ Project2/Project2Website/BinaryDecisionTreeWebsite/src/pages/JustinPage.jsx
@@ -4,7 +4,7 @@ function JustinPage() {
             <h1>Justin's Page is for my visualations and analytics</h1>
             <h2>Visualization:</h2>
             <p>Mean Values: The Average Weekly Death Rate.</p>
-            <img src='./src/assets/ScatterPlotofMeans.png' alt="ScatterPlot" width="500" height="400"/>
+            <img src='./src/assets/ScatterPlotofMeans.png' alt="ScatterPlot" width="800" height="500"/>
             <img src='./src/assets/Top15MeanByJurisdiction.png' alt="Top 15 Mean By Jurisdicition" width="500" height="400"/>
             <img src='./src/assets/10SmallestMeanValuesByJurisdictiom.png' alt="10 Smallest Means" width="500" height="400"/>
             <h2>Analytics</h2>
